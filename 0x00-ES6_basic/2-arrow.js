bdb123a54b51f31775eb6ec545c155d2a9f7f42e
Peter Rigii <theonlyrigii@gmail.com>
2024-01-03 09:35:10 +0300

    arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    this.addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
+  
--- 0x00-ES6_basic/README.md
+++ 0x00-ES6_basic/README.md
@@ -2,3 +2,4 @@
 ## Tasks
 0. Const or let?
 1. Block Scope
+2. Arrow functions
