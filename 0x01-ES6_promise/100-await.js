2d1d95712b2ddc801d834cfa9f293b36f1c0bc61
Peter Rigii <theonlyrigii@gmail.com>
2024-01-03 14:01:33 +0300

    advanced tasks

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+    try {
+    // Use Promise.all to concurrently execute uploadPhoto and createUser
+    const [photoResponse, userResponse] = await Promise.all([
+        uploadPhoto(),
+        createUser(),
+    ]);
+
+    // Return an object with the responses
+    return {
+        photo: photoResponse,
+        user: userResponse,
+    };
+    } catch (error) {
+    // Handle errors and return an empty object
+    console.error('Error during async upload and user creation:', error.message);
+    return {
+        photo: null,
+        user: null,
+    };
+    }
+}
