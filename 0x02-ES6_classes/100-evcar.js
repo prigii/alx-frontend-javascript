6981ac2fb96bf7f85475ee1382d618276ce2a7c4
Peter Rigii <theonlyrigii@gmail.com>
2024-01-07 13:43:12 +0300

    fixed bug

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,13 @@
-import Car from './10-car.js';
-
-const _range = Symbol('range');
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this[_range] = range;
+    this._range = range;
   }
 
-  // Getter method for the encapsulated range attribute
-  get range() {
-    return this[_range];
-  }
-
-  // Override the cloneCar method to return an instance of Car
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    return new Car(this.brand, this.motor, this.color);
+    return new Car();
   }
 }
