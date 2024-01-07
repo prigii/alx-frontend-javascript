2e7bcfd9a3863ff4a6d51af1e79027cce6f12e66
Peter Rigii <theonlyrigii@gmail.com>
2024-01-07 13:10:34 +0300

    debugged

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom.js';
 
 // Function to initialize ClassRoom objects
-function initializeRooms() {
-  const sizes = [19, 20, 34];
-  return sizes.map(size => new ClassRoom(size));
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
