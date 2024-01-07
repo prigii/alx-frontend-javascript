5804763f4e76d252f500257b15552bdd0fd5552f
Peter Rigii <theonlyrigii@gmail.com>
2024-01-07 13:35:34 +0300

    fixed bug

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,42 +1,13 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._setSize(size);
-      this._setLocation(location);
-    }
-  
-    // Getter and Setter for size attribute
-    _getSize() {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
       return this._size;
     }
-  
-    _setSize(size) {
-      if (typeof size === 'number') {
-        this._size = size;
-      } else {
-        throw new TypeError('Size must be a number');
-      }
-    }
-  
-    // Getter and Setter for location attribute
-    _getLocation() {
-      return this._location;
-    }
-  
-    _setLocation(location) {
-      if (typeof location === 'string') {
-        this._location = location;
-      } else {
-        throw new TypeError('Location must be a string');
-      }
-    }
-  
-    // Behavior when cast into a Number
-    valueOf() {
-      return this._getSize();
-    }
-  
-    // Behavior when cast into a String
-    toString() {
-      return this._getLocation();
-    }
+    return this._location;
   }
+}
