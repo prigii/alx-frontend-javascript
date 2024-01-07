e05e3eb424d5310ca6212c76507be27ccca4a724
Peter Rigii <theonlyrigii@gmail.com>
2024-01-07 13:32:00 +0300

    fixed bug

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,24 +1,20 @@
 export default class Building {
-    constructor(sqft) {
-      this._setSqft(sqft);
-    }
-  
-    // Getter for sqft attribute
-    _getSqft() {
-      return this._sqft;
-    }
-  
-    _setSqft(sqft) {
-      if (typeof sqft === 'number') {
-        this._sqft = sqft;
-      } else {
-        throw new TypeError('Sqft must be a number');
-      }
-    }
-  
-    // Abstract method to be implemented by subclasses
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
-  
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
