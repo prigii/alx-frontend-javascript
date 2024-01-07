9b3d83681ccf621dcd7464b0d6061131f7ff6dda
Peter Rigii <theonlyrigii@gmail.com>
2024-01-07 13:23:21 +0300

    debugged

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,37 +1,35 @@
 export default class Currency {
-    constructor(code, name) {
-      this._setCode(code);
-      this._setName(name);
-    }
-  
-    // Getter and Setter for code attribute
-    _getCode() {
-      return this._code;
-    }
-  
-    _setCode(code) {
-      if (typeof code === 'string') {
-        this._code = code;
-      } else {
-        throw new TypeError('Code must be a string');
-      }
-    }
-  
-    // Getter and Setter for name attribute
-    _getName() {
-      return this._name;
-    }
-  
-    _setName(name) {
-      if (typeof name === 'string') {
-        this._name = name;
-      } else {
-        throw new TypeError('Name must be a string');
-      }
-    }
-  
-    // Method to display full currency information
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  // Getter and setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('TypeError: Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('TypeError: Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
