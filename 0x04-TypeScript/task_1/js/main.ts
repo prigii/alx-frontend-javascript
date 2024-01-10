e26b34476f1c4b2fac456a7a6050168f32bdcc7c
Peter Rigii <theonlyrigii@gmail.com>
2024-01-10 19:29:04 +0300

    added teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+// task_1/main.ts
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
