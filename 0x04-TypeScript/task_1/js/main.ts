b1b592d893117545e6b85fa4859162a9aaa49b5f
Peter Rigii <theonlyrigii@gmail.com>
2024-01-10 19:37:51 +0300

    extended teacher class with director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -31,3 +31,14 @@ const director1: Directors = {
 };
 
 console.log(director1);
+s
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${firstInitial}. ${fullLastName}`;
+};
